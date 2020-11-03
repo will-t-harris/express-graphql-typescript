241ac2bfa5943dbb7dafcb56e2cde417852862dd
Will Harris <will.t.harris@protonmail.com>
2020-11-02 21:38:59 -0800

    setup express server on port 5000

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import { __prod__, expressPort } from "./constants";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
-	const orm = await MikroORM.init(microConfig);
-	await orm.getMigrator().up();
-	// const post = orm.em.create(Post, { title: "my first post" });
-	// await orm.em.persistAndFlush(post);
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-	const posts = await orm.em.find(Post, {})
-	console.log(posts)
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(expressPort, () => {
+    console.log(`server started on ${expressPort}`);
+  });
 };
 
 main().catch((err) => console.error(err));
