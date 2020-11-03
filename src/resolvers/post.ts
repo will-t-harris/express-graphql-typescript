f429ba91e64278e813bf09bdd28399872e226983
Will Harris <will.t.harris@protonmail.com>
2020-11-02 22:37:42 -0800

    add get single post query to resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
-  @Query(() => [Post]) // the uppercase String is a type-graphql thing
-  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
-    return em.find(Post, {});
-  }
+	@Query(() => [Post]) // the uppercase String is a type-graphql thing
+	posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+		return em.find(Post, {});
+	}
+
+	@Query(() => Post, { nullable: true })
+	post(
+		@Arg("id", () => Int) id: number | null,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		return em.findOne(Post, { id });
+	}
 }
