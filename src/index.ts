dddbb4a685428dbdfb5d3c2594445503edc45ef6
Will Harris <will.t.harris@protonmail.com>
2020-11-02 21:29:31 -0800

    feat: setup express to run on port 5000

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
 	const orm = await MikroORM.init(microConfig);
 	await orm.getMigrator().up();
-	// const post = orm.em.create(Post, { title: "my first post" });
-	// await orm.em.persistAndFlush(post);
 
-	const posts = await orm.em.find(Post, {})
-	console.log(posts)
+	const app = express();
+	app.get("/", (_, res) => {
+		res.send("hello");
+	});
+	app.listen(5000, () => {
+		console.log("server started on localhost:5000");
+	});
 };
 
 main().catch((err) => console.error(err));
