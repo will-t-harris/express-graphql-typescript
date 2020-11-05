e96911f15fd9829dc3fabf3116c6a8adf7b9af45
Will Harris <will.t.harris@protonmail.com>
2020-11-04 20:17:34 -0800

    add createPost and updatePost mutations

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,10 +1,10 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
-	@Query(() => [Post]) // the uppercase String is a type-graphql thing
+	@Query(() => [Post])
 	posts(@Ctx() { em }: MyContext): Promise<Post[]> {
 		return em.find(Post, {});
 	}
@@ -15,5 +15,32 @@ export class PostResolver {
 		@Ctx() { em }: MyContext
 	): Promise<Post | null> {
 		return em.findOne(Post, { id });
+	}
+
+	@Mutation(() => Post)
+	async createPost(
+		@Arg("title") title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post> {
+		const post = em.create(Post, { title });
+		await em.persistAndFlush(post);
+		return post;
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost(
+		@Arg("id", () => Int) id: number,
+		@Arg("title") title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		const post = await em.findOne(Post, { id });
+		if (!post) {
+			return null;
+		}
+		if (typeof title !== "undefined") {
+			post.title = title;
+			await em.persistAndFlush(post);
+		}
+		return post;
 	}
 }
